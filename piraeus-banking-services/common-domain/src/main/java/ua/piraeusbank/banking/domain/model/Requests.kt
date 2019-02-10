656ffea46b08234964bbbe4861a441b7646c6c70
qnex <qnexdev@gmail.com>
2019-02-10 19:17:41 +0200

    * Major changes

---
+++ piraeus-banking-services/common-domain/src/main/java/ua/piraeusbank/banking/domain/model/Requests.kt
@@ -0,0 +1,26 @@
+package ua.piraeusbank.banking.domain.model
+
+import org.javamoney.moneta.Money
+import ua.piraeusbank.banking.domain.entity.CardNetworkCode
+import java.time.LocalDate
+
+data class MoneyTransferRequest(
+        val sourceAccountId: Long,
+        val targetAccountId: Long,
+        val amount: Money,
+        val description: String?)
+
+data class AccountCreationRequest(
+        val customerId: Long,
+        val currencyCode: String)
+
+
+
+data class OrderCardRequest(val customerId: Long, val accountId: Long, val networkCode: CardNetworkCode)
+
+data class CustomerRegistrationRequest(val phoneNumber: String,
+                                       val email: String,
+                                       val password: String,
+                                       val name: String,
+                                       val lastName: String,
+                                       val dateOfBirthday: LocalDate)
