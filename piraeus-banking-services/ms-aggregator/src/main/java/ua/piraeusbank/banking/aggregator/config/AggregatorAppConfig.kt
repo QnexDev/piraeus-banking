2798f57f765374ff417854069c2419fd6d30fa0e
qnex <qnexdev@gmail.com>
2019-02-12 17:53:15 +0200

    * Major changes

--- piraeus-banking-services/ms-aggregator/src/main/java/ua/piraeusbank/banking/aggregator/config/AggregatorAppConfig.kt
+++ piraeus-banking-services/ms-aggregator/src/main/java/ua/piraeusbank/banking/aggregator/config/AggregatorAppConfig.kt
@@ -2,6 +2,7 @@ package ua.piraeusbank.banking.aggregator.config
 
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
@@ -13,7 +14,7 @@ class AggregatorAppConfig {
     @Bean
     fun objectMapperBuilder(): Jackson2ObjectMapperBuilder {
         val builder = Jackson2ObjectMapperBuilder()
-        builder.modules(Jdk8Module(), JavaTimeModule(), MoneyModule())
+        builder.modules(Jdk8Module(), JavaTimeModule(), MoneyModule(), KotlinModule())
         return builder
     }
 }
